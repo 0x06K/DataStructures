d5aaed8a50588e413d6c6d6148d52eb3571d6f63
archlinuxuser <161124933+0x06K@users.noreply.github.com>
2025-08-29 06:03:45 +0000

    DataStructures

--- nasm/practice/first.asm
+++ nasm/practice/first.asm
@@ -1,21 +1,24 @@
-.text
-global _start
+; Create program with different data sizes (db/dw/dq) and print their values |
+;============================================================================+
+
+.text			; code section whatever is in this section will be executed 
+global _start		; sets _start as the program entry point
 
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, var1
-    mov rdx, 16
-    syscall
-    mov eax, 60
-    xor rdi,rdi
-    syscall 
+    mov rax, 1		; number of syscall <write>
+    mov rdi, 1		; file discripter(fd) for stdout
+    mov rsi, var1 	; pointer to buffer
+    mov rdx, 2		; length of string
+    syscall		; cpu switches from usermode to kernel mode
 
-.data
-var1 DB "Hello Friend !!",10,0
+    ; return 0
+    mov eax, 60		; syscall number of <exit>
+    xor rdi,rdi		; 0 for success
+    syscall		; transition from user mode -> kernel mode
 
+.data			; only initialized data
 
-
+var1 DB "0",10 		;10 is the ascii of newline character
 
 
 
